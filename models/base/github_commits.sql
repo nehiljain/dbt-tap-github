64cb4697509bac4e24825c5af6385aaf7607a50f
Félix Lehoux <felix.lehoux@hotmail.com>
2020-02-19 17:24:56 -0500

    Fix columns

--- models/base/github_commits.sql
+++ models/base/github_commits.sql
@@ -30,26 +30,33 @@ renamed as (
 */
 
         _sdc_repository as _sdc_repository,
-        commit_tree as commit_tree,
-        comments_url as comments_url,
-        commit_message as commit_message,
-        commit_committer_date as commit_committer_date,
-        commit_tree_url as commit_tree_url,
-        commit_comment_count as commit_comment_count,
-        parents as parents,
-        commit_committer_name as commit_committer_name,
-        commit_author as commit_author,
+
+        /* commit_author as commit_author, */
         commit_author_date as commit_author_date,
-        sha as sha,
-        commit_author_name as commit_author_name,
-        commit_tree_sha as commit_tree_sha,
-        html_url as html_url,
-        commit_url as commit_url,
-        commit_committer_email as commit_committer_email,
-        url as url,
         commit_author_email as commit_author_email,
-        commit_committer as commit_committer,
-        commit as commit
+        commit_author_name as commit_author_name,
+
+        commit_comment_count as commit_comment_count,
+
+        /* commit_committer as commit_committer, */
+        commit_committer_date as commit_committer_date,
+        commit_committer_email as commit_committer_email,
+        commit_committer_name as commit_committer_name,
+
+        commit_message as commit_message,
+
+        /* commit_tree as commit_tree, */
+        commit_tree_sha as commit_tree_sha,
+        commit_tree_url as commit_tree_url,
+        commit_url as commit_url,
+
+        html_url as html_url,
+        parents as parents,
+        sha as sha,
+        url as url,
+
+        /* comments_url as comments_url, */
+        /* commit as commit */
 
     from source
 
