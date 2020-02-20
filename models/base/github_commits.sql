4a338c73ee02a51ad0a83ff1d217543fe956ae74
Félix Lehoux <felix.lehoux@hotmail.com>
2020-02-20 10:13:00 -0500

    trying have the right synthax

--- models/base/github_commits.sql
+++ models/base/github_commits.sql
@@ -7,22 +7,25 @@ with source as (
 renamed as (
 
     select
-        _sdc_repository as _sdc_repository,
-        commit__author__date as commit__author__date,
-        commit__author__email as commit__author__email,
-        commit__author__name as commit__author__name,
-        commit__comment__count as commit__comment__count,
-        commit__committer__date as commit__committer__date,
-        commit__committer__email as commit__committer__email,
-        commit__committer__name as commit__committer__name,
-        commit__message as commit__message,
-        commit__tree__sha as commit__tree__sha,
-        commit__tree__url as commit__tree__url,
-        commit__url as commit__url,
-        html__url as html__url,
-        parents as parents,
-        sha as sha,
+
+        _sdc_repository as _sdc_repository
+        comments_url as comments_url
+        commit__message as commit__message
+        commit__committer__date as commit__committer__date
+        commit__tree__url as commit__tree__url
+        commit__comment_count as commit__comment_count
+        parents as parents
+        commit__committer__name as commit__committer__name
+        #author as #author
+        commit__author__date as commit__author__date
+        sha as sha
+        commit__author__name as commit__author__name
+        commit__tree__sha as commit__tree__sha
+        html_url as html_url
+        commit__url as commit__url
+        commit__committer__email as commit__committer__email
         url as url
+        commit__author__email as commit__author__email
 
     from source
 
