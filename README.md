7d748fcded1dacf383cc2c1b2510adf8d0f089c8
Félix Lehoux <felix.lehoux@hotmail.com>
2020-02-20 10:58:06 -0500

    Change as name (simplest)

--- README.md
+++ README.md
@@ -1,21 +1,19 @@
-# dbt | tap-gitlab
+# dbt | tap-github
 
-This [dbt](https://github.com/fishtown-analytics/dbt) package contains data models for [tap-gitlab](https://gitlab.com/meltano/tap-gitlab).
+This [dbt](https://github.com/fishtown-analytics/dbt) package contains data models for [tap-github](https://github.com/singer-io/tap-github).
 
 Two variables are required:
-* schema: the schema where the raw gitlab tables (result of `tap-gitlab`) are located
-* ultimate_license: If it is true, then the GitLab account used has access to the Gitlab Ultimate or Gitlab.com Gold features. It will enable models over tables that are only available for Gitlab Ultimate and Gitlab.com Gold accounts. For example, `epics` and `epic_issues`.
+* schema: the schema where the raw github tables (result of `tap-github`) are located
 
 **dbt_project.yml**
 ```
 ... ...
 
 models:
-  tap_gitlab:
+  tap_github:
 
     ... ...
 
     vars:
-      schema:             'tap_gitlab'        # the schema where the raw gitlab tables are located
-      ultimate_license:   'true'        # whether tables (e.g. epics) only available on Ultimate Accounts are available
+      schema:             'tap_github'        # the schema where the raw github tables are located
 ```
--- models/base/github_commits.sql
+++ models/base/github_commits.sql
@@ -10,21 +10,21 @@ renamed as (
 
         _sdc_repository as _sdc_repository,
         comments_url as comments_url,
-        commit__message as commit__message,
-        commit__committer__date as commit__committer__date,
-        commit__tree__url as commit__tree__url,
-        commit__comment_count as commit__comment_count,
+        commit__message as message,
+        commit__committer__date as committer_date,
+        commit__tree__url as tree_url,
+        commit__comment_count as comment_count,
         parents as parents,
-        commit__committer__name as commit__committer__name,
-        commit__author__date as commit__author__date,
+        commit__committer__name as committer_name,
+        commit__author__date as author_date,
         sha as sha,
-        commit__author__name as commit__author__name,
-        commit__tree__sha as commit__tree__sha,
+        commit__author__name as author_name,
+        commit__tree__sha as tree_sha,
         html_url as html_url,
-        commit__url as commit__url,
-        commit__committer__email as commit__committer__email,
+        commit__url as commit_url,
+        commit__committer__email as committer_email,
         url as url,
-        commit__author__email as commit__author__email
+        commit__author__email as author_email
 
     from source
 
